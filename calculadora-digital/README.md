7e57942ad301a957ae1b90c29f77c3d014f2c2ef
Liam <liamnahuelromero@gmail.com>
2025-05-21 18:14:34 -0300

    se crea carpeta para calculadora digital

---
+++ calculadora-digital/README.md
@@ -0,0 +1,2 @@
+# calculadora-digital
+Un trabajo practico simple, arme una calculadora en JavaScript, y seguramente me gustaría darle un poco de diseño con HTML y CSS a futuro.
---
+++ calculadora-digital/calculadora.js
@@ -0,0 +1,66 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+function suma(num1, num2) {
+    return num1 + num2;
+}
+
+function resta(num1, num2) {
+    return num1 - num2;
+}
+
+function multiplicacion(num1, num2) {
+    return num1 * num2;
+}
+
+function division(num1, num2) {
+    if (num2 == 0) {
+        return "Ingresa un número valido";
+    } else {
+        return num1 / num2;
+    }
+}
+
+console.log("Bienvenido a la calculadora digital");
+console.log("1. Suma");
+console.log("2. Resta");
+console.log("3. Multiplicación");
+console.log("4. División");
+
+let opción = parseInt(prompt("Indique la operación que quiera realizar:"));
+let numero1;
+let numero2;
+let rdo;
+switch (opción) {
+    case 1:
+        numero1 = parseInt(prompt("Numero 1: "));
+        numero2 = parseInt(prompt("Numero 2: "));
+        rdo = suma(numero1, numero2);
+        console.log("El resultado es: " + rdo)
+        break;
+    case 2:
+        numero1 = parseInt(prompt("Numero 1: "));
+        numero2 = parseInt(prompt("Numero 2: "));
+        rdo = resta(numero1, numero2);
+        console.log("El resultado es: " + rdo)
+        break;
+    case 3:
+        numero1 = parseInt(prompt("Numero 1: "));
+        numero2 = parseInt(prompt("Numero 2: "));
+        rdo = multiplicacion(numero1, numero2);
+        console.log("El resultado es: " + rdo)
+        break;
+    case 4:
+        numero1 = parseInt(prompt("Numero 1: "));
+        numero2 = parseInt(prompt("Numero 2: "));
+        rdo = division(numero1, numero2);
+        if (typeof rdo === "string") {
+            console.log(rdo)
+        }
+        else {
+            console.log("El resultado es: " + rdo)
+        }
+        break;  
+    default:
+        console.log("Opción no válida.");
+        break;
+}
