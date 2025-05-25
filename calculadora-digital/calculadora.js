54db9df542c78f8f284df297bd410262263f32d7
Liam <liamnahuelromero@gmail.com>
2025-05-25 17:48:23 -0300

    se agregan saltos de lineas y colores en la respuesta por consola para una mejor experiencia de usuario

--- calculadora-digital/calculadora.js
+++ calculadora-digital/calculadora.js
@@ -26,8 +26,9 @@ function potencia(num1, num2){
 
 let historial = [] 
 
+console.log("Bienvenido a la calculadora digital");
+
 while (true) {
-    console.log("Bienvenido a la calculadora digital");
     console.log("1. Suma");
     console.log("2. Resta");
     console.log("3. Multiplicación");
@@ -52,22 +53,22 @@ while (true) {
             numero1 = parseInt(prompt("Numero 1: "));
             numero2 = parseInt(prompt("Numero 2: "));
             rdo = suma(numero1, numero2);
-            console.log("El resultado es: " + rdo)
-            historial.push(`Suma: ${numero1} + ${numero2} = ${rdo}`);
+            console.log("\x1b[32m%s\x1b[0m","El resultado es: " + rdo);
+            historial.push(`\nSuma: \n${numero1} + ${numero2} = ${rdo}`);
             break;
         case 2:
             numero1 = parseInt(prompt("Numero 1: "));
             numero2 = parseInt(prompt("Numero 2: "));
             rdo = resta(numero1, numero2);
-            console.log("El resultado es: " + rdo)
-            historial.push(`resta: ${numero1} - ${numero2} = ${rdo}`);
+            console.log("\x1b[32m%s\x1b[0m","El resultado es: " + rdo);
+            historial.push(`\nresta: \n${numero1} - ${numero2} = ${rdo}`);
             break;
         case 3:
             numero1 = parseInt(prompt("Numero 1: "));
             numero2 = parseInt(prompt("Numero 2: "));
             rdo = multiplicacion(numero1, numero2);
-            console.log("El resultado es: " + rdo)
-            historial.push(`Multipliación: ${numero1} * ${numero2} = ${rdo}`);
+            console.log("\x1b[32m%s\x1b[0m","El resultado es: " + rdo);
+            historial.push(`\nMultipliación: \n${numero1} * ${numero2} = ${rdo}`);
             break;
         case 4:
             numero1 = parseInt(prompt("Numero 1: "));
@@ -77,26 +78,26 @@ while (true) {
             console.log(rdo);
             }
             else {
-            console.log("El resultado es: " + rdo);
-            historial.push(`División: ${numero1} / ${numero2} = ${rdo}`);
+            console.log("\x1b[32m%s\x1b[0m","El resultado es: " + rdo);
+            historial.push(`\nDivisión: \n${numero1} / ${numero2} = ${rdo}`);
             }
             break;
         case 5:
             numero1 = parseInt(prompt("Numero 1: "));
             numero2 = parseInt(prompt("Numero 2: ")); 
             rdo = potencia(numero1, numero2);
-            console.log("El resultado es: "+ rdo)
-            historial.push(`Potencia: ${numero1} ** ${numero2} = ${rdo}`);
+            console.log("\x1b[32m%s\x1b[0m","El resultado es: "+ rdo);
+            historial.push(`\nPotencia: \n${numero1} ** ${numero2} = ${rdo}`);
             break;
         case 6: 
             if (historial.length === 0) {
-                console.log("No hay operaciones registradas en el historial.")
+                console.log("No hay operaciones registradas en el historial.");
             } else {
-                console.log("Historial de operaciones: " + historial);
+                console.log("\x1b[91m%s\x1b[0m","Historial de operaciones: " + historial);
             }
             break
         default:
-            console.log("Opción no válida.");
+            console.log("Opción no válida. Elija un Número del 1 al 6 o 0 para Salir.");
             break;
     }
 }
